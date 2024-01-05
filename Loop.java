72156077648af91bea92514bf23ed015fa8040b9
Richa Raj <110665175+123richa-raj@users.noreply.github.com>
2024-01-05 22:09:27 +0530

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,11 @@
+package Lec_6;
+
+public class Loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (byte i = 0; i < 127; i++) {
+			System.out.println(i);
+		}
+	}
+}
