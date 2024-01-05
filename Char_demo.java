abfaf1ae2ec17b32652cc44c28c230ea6c232ad6
Richa Raj <110665175+123richa-raj@users.noreply.github.com>
2024-01-05 22:07:31 +0530

    Add files via upload

---
+++ Char_demo.java
@@ -0,0 +1,28 @@
+package Lec_6;
+import java.util.*;
+
+public class Char_demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch = 'a';
+		char ch1 = '?';
+		Scanner sc = new Scanner(System.in);
+		ch = sc.next().charAt(0);// input
+
+		System.out.println(ch);
+		System.out.println(ch1);
+		System.out.println((int) (ch));
+		System.out.println((int) (ch1));
+		System.out.println(ch + ch1);
+		int x = 19;
+		x++;// (int)(x+1)//implicit
+		++ch;// ch = (char)(ch+1)//implicit
+		System.out.println(ch);
+		ch = (char) (ch + 1);// Explicit
+		ch += 1;// ch= (char)(ch+1); //implicit
+		System.out.println(ch);
+		char g = (char)(ch+ch1);
+		System.out.println(g);
+	}
+}
